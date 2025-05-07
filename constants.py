29185365ffeab69744a7f78f242cb3850e4ecd04
sashkotenk <oleksandr_bondarchuk@knu.ua>
2025-05-07 15:43:05 +0300

    додано константи

---
+++ constants.py
@@ -0,0 +1,13 @@
+TILE      = 24          # px
+FPS       = 60
+FONT_NAME = "arcade.ttf"  # покладіть файл у assets/
+
+# Кольори (R, G, B)
+BLACK  = (  0,   0,   0)
+WHITE  = (255, 255, 255)
+RED    = (255,   0,   0)
+PINK   = (255, 182, 193)
+CYAN   = (  0, 255, 255)
+ORANGE = (255, 165,   0)
+BLUE   = (  0,   0, 255)
+YELLOW = (255, 255,   0)
